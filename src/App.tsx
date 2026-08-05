da4b705c8f93e0c22e3e78ef93ac4a3816a81ba0
KauaGabriell <kauagabriell64@gmail.com>
2026-08-05 15:22:19 -0300

    feat(icon): add SVG icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import TrashIcon from "../src/assets/icons/Trash-Regular.svg?react";
+import { Icon } from "./components/Icon";
 import { Text } from "./components/Text";
 export default function App() {
-	return <Text variant="body-md-semibold">Hello World</Text>;
+	return (
+		<div>
+			<Text variant="body-md-semibold">Hello World</Text>
+			<Icon svg={TrashIcon}/>
+		</div>
+	);
 }
---
+++ src/components/Icon.tsx
@@ -0,0 +1,30 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import type React from "react";
+
+export const IconVariants = cva("", {
+	variants: {
+		animate: {
+			false: "",
+			true: "animate-spin",
+		},
+	},
+	defaultVariants: {
+		animate: false,
+	},
+});
+
+type IconProps = React.ComponentProps<"svg"> &
+	VariantProps<typeof IconVariants> & {
+		svg: React.FC<React.ComponentProps<"svg">>;
+	};
+
+export function Icon({
+	svg: SvgComponent,
+	animate,
+	className,
+	...props
+}: IconProps) {
+	return (
+		<SvgComponent className={IconVariants({ animate, className })} {...props} />
+	);
+}
