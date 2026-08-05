412c861b4024627184bb4ea35e1509decf7328ba
KauaGabriell <kauagabriell64@gmail.com>
2026-08-05 16:34:12 -0300

    feat(icon-button): add icon action variants

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TrashIcon from "../src/assets/icons/Trash-Regular.svg?react";
 import { Badge } from "./components/Badge";
 import { Button } from "./components/Button";
 import { Icon } from "./components/Icon";
+import { IconButton } from "./components/IconButton";
 import { Text } from "./components/Text";
 export default function App() {
 	return (
@@ -12,6 +13,9 @@ export default function App() {
 			<Badge variant={"primary"}>Testando</Badge>
 			<Badge variant={"secondary"}>Testando</Badge>
 			<Button icon={Plus}> Nova Tarefa</Button>
+			<IconButton icon={TrashIcon} variant={'primary'}/>
+			<IconButton icon={TrashIcon} variant={'secondary'}/>
+			<IconButton icon={TrashIcon} variant={'terciary'}/>
 		</div>
 	);
 }
---
+++ src/components/IconButton.tsx
@@ -0,0 +1,64 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import type React from "react";
+import { Icon, type IconProps } from "./Icon";
+
+export const IconButtonVariants = cva(
+	"inline-flex items-center justify-center transition cursor-pointer group",
+	{
+		variants: {
+			variant: {
+				primary: "bg-green-base hover:bg-green-dark",
+				secondary: "bg-gray-200 hover:bg-pink-base",
+				terciary: "bg-transparent hover:bg-gray-200",
+			},
+			size: {
+				sm: "w-6 h-6 p-1 rounded",
+			},
+			disabled: {
+				true: "opacity-50 pointer-events-none",
+			},
+		},
+		defaultVariants: {
+			variant: "primary",
+			size: "sm",
+			disabled: false,
+		},
+	},
+);
+
+export const IconIconButtonVariants = cva("", {
+	variants: {
+		variant: {
+			primary: "fill-white",
+			secondary: "fill-pink-base group-hover:fill-white",
+			terciary: "fill-gray-300 group-hover:fill-gray-400",
+		},
+		size: {
+			sm: "h-4 w-4",
+		},
+	},
+	defaultVariants: { variant: "primary", size: "sm" },
+});
+
+export type IconButtonProps = React.ComponentProps<"button"> &
+	VariantProps<typeof IconButtonVariants> & {
+		icon: IconProps["svg"];
+	};
+
+export function IconButton({
+	variant,
+	size,
+	disabled,
+	className,
+	icon,
+	...props
+}: IconButtonProps) {
+	return (
+		<button
+			className={IconButtonVariants({ variant, size, disabled, className })}
+			{...props}
+		>
+			<Icon svg={icon} className={IconIconButtonVariants({ variant, size })} />
+		</button>
+	);
+}
