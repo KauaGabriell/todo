48fc95f585147445f0d51f96c5d3cc5f132b5d2a
KauaGabriell <kauagabriell64@gmail.com>
2026-08-07 14:46:16 -0300

    feat(home): add task summary

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { Container } from "../components/Container";
+import { TasksSummary } from "../components/core-components/TasksSummary";
 export function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<Container as="article" className="space-y-3">
+			<header className="flex items-center justify-between">
+				<TasksSummary />
+			</header>
+		</Container>
 	);
 }
